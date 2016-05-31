6660ac228c17186e3e90b71549cb92826b85f8d4
Jonathan ABOUT <jonathanabout.pro@gmail.com>
2016-05-31 18:29:44 +0200

    Install/Config Angular, Gulp, Materialize, SASS

---
+++ app/app.js
@@ -0,0 +1,1 @@
+var frontApp = angular.module('frontApp', []);
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var minifycss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css', function () {
+    return gulp.src('./sass/styles.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(rename('styles-unminified.css'))
+        .pipe(gulp.dest('./css/'))
+        .pipe(rename('styles.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['./sass/*.scss'], ['css']);
+    gulp.watch(['./sass/pages/*.scss'], ['css']);
+});
+
+gulp.task('default', ['css']);
